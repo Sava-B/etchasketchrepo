e78018059f582e3f18780041bd41ef34999f1bb0
Sava <savab2164@gmail.com>
2022-07-30 21:59:18 -0600

    edit

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const container = document.querySelector('#grid');
 const btn = document.querySelector('#btn');
 const inpt = document.querySelector("#inpt");
 
-
+const reset = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
 
 btn.addEventListener('click', () => {
     if (typeof parseInt(inpt.value) == 'number' && parseInt(inpt.value) < 100 && parseInt(inpt.value) > 0) {
         let timeout;
+        reset(container)
         for (let i = 0; i < parseInt(inpt.value)**2; i++) {
             const div = document.createElement('div');
             div.className = 'divs';
@@ -27,11 +32,11 @@ btn.addEventListener('click', () => {
                 timefunction()
                 
             })
-        
         }
-        
+
     } else {
-        alert('You need to input an integer');        
+        alert('You need to input an integer');
+        reset(container) 
     }
 })
 
